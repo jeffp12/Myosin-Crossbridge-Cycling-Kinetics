31d9aa82f394f328b59f93906649f6d6c6d5bce3
jeffp12 <jeffp12@uw.edu>
2024-12-04 17:12:26 -0800

    Update percent_change_calc_plot.py

--- src/percent_change_calc_plot.py
+++ src/percent_change_calc_plot.py
@@ -1,39 +1,26 @@
-# This function calculates perfect change for my produced arrays and returns them in their individual arrays
-def calculate_percent_change(control, perturbed):
+def calculate_and_plot_percent_changes(wildtype, perturbed, cb_labels):
+    # This function serves to calculate percent changes between the wildtype and perturbed results and then produces a simple 2D visualiztion
+    
     with np.errstate(divide='ignore', invalid='ignore'):  # Ignore divide-by-zero and invalid warnings
-        percent_change = (perturbed - control) / perturbed * 100
-        percent_change[np.isinf(percent_change)] = np.nan  # Replace infinite values with NaN
-    return percent_change
+        percent_changes = (perturbed - wildtype) / perturbed * 100
+        percent_changes[np.isinf(percent_changes)] = np.nan  # Replace infinite values with NaN
 
-control = np.array([CB0, CB1, CB2, CB3, CB4, CB5, CB6])
-perturbed = np.array([NewCB0, NewCB1, NewCB2, NewCB3, NewCB4, NewCB5, NewCB6])
-
-percent_changes = []
-for cb_control, cb_perturbed in zip(control, perturbed):
-    percent_changes.append(calculate_percent_change(cb_control, cb_perturbed))
-
-percent_changes = np.array(percent_changes)
-print("Percent Changes for all CBs:", percent_changes)
-
-
-# Producing a visualization showing the percent change of each species 
-cb_labels = ['CB0', 'CB1', 'CB2', 'CB3', 'CB4', 'CB5', 'CB6']
-
-def plot_percent_changes(percent_changes, cb_labels):
+    # Visualizing 
     rows_CBs = percent_changes.shape[0]
-    plt.figure(figsize=(7, 4))
+    plt.figure(figsize=(9, 7))
 
     for i in range(rows_CBs):
         plt.plot(
-            percent_changes[i], 
+            percent_changes[i],
             label=cb_labels[i]
-            )
-    
-    plt.title("% change of Control vs Pertrubed CB species")
-    plt.xlabel(" # of Data Points")
+        )
+
+    plt.title("% Change of wildtype vs Perturbed CB Species")
+    plt.xlabel("# of Data Points")
     plt.ylabel("Percent Change (%)")
     plt.legend(title="CB Species")
     plt.axhline(0, color='black', linewidth=1, linestyle='--')  # Reference line at 0
     plt.grid(True)
+    plt.show()
 
-plot_percent_changes(percent_changes, cb_labels)
+    return percent_changes
