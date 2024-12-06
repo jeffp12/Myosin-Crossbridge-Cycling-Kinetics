398328a6a4650f3336c023817e97ebf980a34d5f
jeffp12 <jeffp12@uw.edu>
2024-12-06 10:35:02 -0800

    Update myosinCK_test.py

--- src/functional_test/myosinCK_test.py
+++ src/functional_test/myosinCK_test.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class TestCrossBridgeDynamics(unittest.TestCase):
     def biochemrxn(self):
-        self.r = te.loada('''
+        self.MODEL = te.loada('''
         J0: CB0 -> CB1; (k1 * CB0 + k20 * CB2 - (k10 + k2) * CB1);
         J1: CB1 -> CB2; (k2 * CB1 + k30 * CB3 - (k20 + k3) * CB2);
         J2: CB2 -> CB3; (k3 * CB2 + k40 * CB4 - (k30 + k4) * CB3);
@@ -34,24 +34,24 @@ class TestCrossBridgeDynamics(unittest.TestCase):
         self.biochemrxn()  
         time_end = 0.3
         num_points = 500
-        m = self.r.simulate(0, time_end, num_points)
+        m = self.MODEL.simulate(0, time_end, num_points)
         self.assertEqual(m.shape, (num_points, 8))  # 8 columns: time + 7 reactants
 
     def test_initial_concentrations(self):
       # Verify initial conditions. CHANGING THIS IN CODE WILL NEED TO BE CHANGED HERE FOR TEST
         self.biochemrxn()  
-        self.assertAlmostEqual(self.r["CB0"], 0.478)
-        self.assertAlmostEqual(self.r["CB1"], 0.0)
-        self.assertAlmostEqual(self.r["CB2"], 0.014)
-        self.assertAlmostEqual(self.r["CB3"], 0.143)
-        self.assertAlmostEqual(self.r["CB4"], 0.0)
-        self.assertAlmostEqual(self.r["CB5"], 0.144)
-        self.assertAlmostEqual(self.r["CB6"], 0.221)
+        self.assertAlmostEqual(self.MODEL["CB0"], 0.478)
+        self.assertAlmostEqual(self.MODEL["CB1"], 0.0)
+        self.assertAlmostEqual(self.MODEL["CB2"], 0.014)
+        self.assertAlmostEqual(self.MODEL["CB3"], 0.143)
+        self.assertAlmostEqual(self.MODEL["CB4"], 0.0)
+        self.assertAlmostEqual(self.MODEL["CB5"], 0.144)
+        self.assertAlmostEqual(self.MODEL["CB6"], 0.221)
 
     def test_concentrations_total(self):
        # Again these are proportion to 1 so all the concentrations at any given time point must still yield 1.
         self.biochemrxn()  
-        m = self.r.simulate(0, 0.3, 500)
+        m = self.MODEL.simulate(0, 0.3, 500)
         total_concentration = np.sum(m[:, 1:], axis=1) # this does the reactant coloumns and doesnt include time coloiumn
         for value in total_concentration:
             self.assertAlmostEqual(value, 1.0, places=3) # my intial conditions used 3 decimal places so I just set 3 here but this could be even higher for more exact approximation
