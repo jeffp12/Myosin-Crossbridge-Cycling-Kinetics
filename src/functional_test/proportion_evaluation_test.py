3b26427971571ec26f6ab299496c32ee387ee935
jeffp12 <jeffp12@uw.edu>
2024-12-06 10:36:15 -0800

    Update proportion_evaluation_test.py

--- src/functional_test/proportion_evaluation_test.py
+++ src/functional_test/proportion_evaluation_test.py
@@ -10,16 +10,16 @@ CB3 = 0.143
 CB4 = 0.0
 CB5 = 0.144
 CB6 = 0.221
-total = CB0 + CB1 + CB2 + CB3 + CB4 + CB5 + CB6
+TOTAL = CB0 + CB1 + CB2 + CB3 + CB4 + CB5 + CB6
 
 # Test if the total is approximately 1 (accounting for floating-point precision)
-if abs(total - 1) == 0:  
+if abs(TOTAL - 1) == 0:  
     print("The variables sum to 1. Test passed!")
 else:
-    print(f"Oh no, test failed! The variables do not sum to 1. Total = {total}")
+    print(f"Oh no, test failed! The variables do not sum to 1. Total = {TOTAL}")
 
 # Fucntional test to make sure the proportions add up to 1
-new_total = new_CB0 + new_CB1 + new_CB2 + new_CB3 + new_CB4 + new_CB5 + new_CB6
+NEW_TOTAL = new_CB0 + new_CB1 + new_CB2 + new_CB3 + new_CB4 + new_CB5 + new_CB6
 # Print out the new values for adjustment to code parameters
 new_conditions = {
     "NewCB0": new_CB0,
@@ -29,7 +29,7 @@ new_conditions = {
     "NewCB4": new_CB4,
     "NewCB5": new_CB5,
     "NewCB6": new_CB6,
-    "New Total": new_total
+    "New Total": NEW_TOTAL
 }
 NewCB = pd.DataFrame(new_conditions.items(), columns=["State", "Value"])
 print(NewCB)
